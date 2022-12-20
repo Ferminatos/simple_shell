84e29de38baf82966ccaf709da24327a2e643568
NunaTna <tseganesh.abera@outlook.com>
2022-12-20 12:17:40 -0800

    Fix

--- shell.h
+++ shell.h
@@ -56,3 +56,18 @@ typedef struct liststr
  * @argv: an aarray of strings generated from arg
  * @path: a string path for the current command
  * @argc: the argument count
+ * @line_count: the error count
+ * @err_num: the error code for exit()s
+ * @linecount_flag: if on count this line of input
+ * @fname: the program filename
+ * @env: linked list local copy of environ
+ * @history: the history node
+ * @alias: the alias node
+ * @env_changed: on if environ was changed
+ * @status: the return status of the last executed command
+ * @cmd_buf: address of pointer to cmd_buf
+ * @cmd_buf_type: CMD_type ||, &&, ;
+ * @histcount: the hsitory line number count
+ */
+
+
