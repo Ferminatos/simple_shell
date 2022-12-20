13ef501eee887cacced1d5520a338a37b78ffba1
NunaTna <tseganesh.abera@outlook.com>
2022-12-20 11:18:50 -0800

    Fix

--- main.c
+++ main.c
@@ -1,32 +1,40 @@
-#include "shell.h"
+#include "main.h"
 
 /**
- * main - Entry point
+ * main - entry point
  * @ac: arg count
  * @av: arg vector
  *
  * Return: 0 on success, 1 on error
  */
 
-int main(int ac, char**av)
+int main(int ac, char **av)
 {
-	info_t info[] = {INFO_INIT};
-	int fd = 2:
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
-		asm("mov %1, %0\n\t"
-				"add $3, %0"
-				:"=r" (fd)
-				:"r" (fd));
+	asm ("mov %1, %0\n\t"
+			"add $3, %0"
+			: "=r" (fd)
+			: "r" (fd));
+
 	if (ac == 2)
 	{
-		fd = open(av[1], O-RDONLY);
+		fd = open(av[1], O_RDONLY);
 		if (fd == -1)
 		{
 			if (errno == EACCES)
 				exit(126);
-			if(errno == Enoent)
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				exit(127);
+			}
+			return (EXIT_FAILURE);
 		}
-
+		info->readfd = fd;
 	}
-	return (EXIT_FAILURE);
 }
